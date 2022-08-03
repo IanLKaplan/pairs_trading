f5d15a59bccf6ec5950408375fd3e2fc6ae6f86d
Ian Kaplan <iank@bearcave.com>
2022-08-03 16:22:53 -0400

    More discussion.

---
+++ s_and_p_filter.py
@@ -0,0 +1,11 @@
+
+from datetime import datetime, timedelta
+from tabulate import tabulate
+from typing import List, Tuple
+from pandas_datareader import data
+import matplotlib.pyplot as plt
+import pandas as pd
+from pandas.core.indexes.datetimes import DatetimeIndex
+import numpy as np
+from pathlib import Path
+import tempfile
