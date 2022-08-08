3b638e7d4dbff09a814ca6571e544b18790f6de3
Ian Kaplan <iank@bearcave.com>
2022-08-08 10:57:45 -0400

    Finished the S and P stock filter code and added the data from barchart.com

--- s_and_p_filter.py
+++ s_and_p_filter.py
@@ -1,74 +1,17 @@
-from datetime import datetime, timedelta
-from tabulate import tabulate
-from typing import List, Tuple, TypedDict, Dict
-from enum import Enum
-from pandas_datareader import data
-import matplotlib.pyplot as plt
-import pandas as pd
-from pandas.core.indexes.datetimes import DatetimeIndex
-import numpy as np
-from pathlib import Path
-import tempfile
 import os
 
+import pandas as pd
+
 #
-# Read a set of files listing S&P 500 sector stocks.  Filter out duplicates (where a stock is in more than one
-# sector. Write out a new CSV file that contains the stock systems, the company name and the sector.
+# Build a single file that contains the S&P 500 stocks and sectors. This file is the base input to the pairs
+# trading notebook.
 #
-s_and_p_directory = '/home/iank/Documents/finance/pairs_trading/s_and_p_sector_components'
-
-
-class Sector(Enum):
-    CONSUMER_DISCRETIONARY = 1
-    CONSUMER_STAPLES = 2
-    ENERGIES = 3
-    FINANCIALS = 4
-    HEALTH_CARE = 5
-    INDUSTRIALS = 6
-    INFO_TECH = 7
-    MATERIALS = 8
-    REAL_ESTATE = 9
-    COMMUNICATION_SVCS = 10
-    UTILITIES = 11
-
-
-enum2str: dict = {Sector.CONSUMER_DISCRETIONARY: 'consumer-discretionary',
-                  Sector.CONSUMER_STAPLES: 'consumer-staples',
-                  Sector.ENERGIES: 'energies',
-                  Sector.FINANCIALS: 'financials',
-                  Sector.HEALTH_CARE: 'health-care',
-                  Sector.INDUSTRIALS: 'industrials',
-                  Sector.INFO_TECH: 'information-technology',
-                  Sector.MATERIALS: 'materials',
-                  Sector.REAL_ESTATE: 'real-estate',
-                  Sector.COMMUNICATION_SVCS: 'communication-services',
-                  Sector.UTILITIES: 'utilities'}
-
-str2enum: dict = {'consumer-discretionary': Sector.CONSUMER_DISCRETIONARY,
-                  'consumer-staples': Sector.CONSUMER_STAPLES,
-                  'energies': Sector.ENERGIES,
-                  'financials': Sector.FINANCIALS,
-                  'health-care': Sector.HEALTH_CARE,
-                  'industrials': Sector.INDUSTRIALS,
-                  'information-technology': Sector.INFO_TECH,
-                  'materials': Sector.MATERIALS,
-                  'real-estate': Sector.REAL_ESTATE,
-                  'communication-services': Sector.COMMUNICATION_SVCS,
-                  'utilities': Sector.UTILITIES}
-
-
-def enum_to_str(enum_val: Sector):
-    return enum2str[enum_val]
-
-
-def str_to_enum(name: str):
-    return str2enum[name]
-
-
-class StockInfo(TypedDict):
-    symbol: str
-    company_name: str
-    sector: Sector
+# To build the file, read a set of files listing the S&P 500 sector stocks.  These files were downloaded over a
+# period of three days from barchart.com.  Write out a new CSV file that contains the stock symbol, the company name
+# and the sector.
+#
+s_and_p_directory = 's_and_p_sector_components'
+output_file_name = 'sp_stocks.csv'
 
 
 def get_sector_name(file_name: str) -> str:
@@ -92,7 +35,6 @@ def process_file(path: str, file_name: str) -> pd.DataFrame:
 
     :param path: the path to the directory containing the file
     :param file_name: the file name
-    :param symbols: a dictionary that will be populated with the data
     :return: Nothing
     """
     sec_name: str = get_sector_name(file_name)
@@ -101,16 +43,12 @@ def process_file(path: str, file_name: str) -> pd.DataFrame:
     # The last row of the data downloaded from barchart contains the line
     # "Downloaded from Barchart.com as of 08-02-2022 10:54am CDT"  Remove this line
     # if it exists
-    last_line = sector_raw.iloc[-1,:][0]
+    last_line = sector_raw.iloc[-1, :][0]
     if type(last_line) == str:
-        if sector_raw.iloc[-1,:][0].startswith('Downloaded'):
+        if sector_raw.iloc[-1, :][0].startswith('Downloaded'):
             sector_raw = sector_raw.head(sector_raw.shape[0] - 1)
-    else:
-        pass
-    sector_df = sector_raw[['Symbol','Name']].copy()
-    sec_enum = str_to_enum(sec_name)
-    l = [sec_enum] * sector_df.shape[0]
-    sector_df['Sector'] = l
+    sector_df = sector_raw[['Symbol', 'Name']].copy()
+    sector_df['Sector'] = sec_name
     return sector_df
 
 
@@ -118,9 +56,11 @@ def process_files(path: str) -> pd.DataFrame:
     s_and_p_df = pd.DataFrame()
     if os.access(path, os.R_OK):
         for file_name in os.listdir(path):
-            sector_df = process_file(path, file_name)
-            s_and_p_df = pd.concat([s_and_p_df, sector_df], axis=0)
-        s_and_p_df.to_csv(path + os.path.sep + 'foo')
+            prefix: str = file_name[0:13]
+            if prefix == 'sp-sectors---':
+                sector_df = process_file(path, file_name)
+                s_and_p_df = pd.concat([s_and_p_df, sector_df], axis=0)
+        s_and_p_df.to_csv(path + os.path.sep + output_file_name)
     else:
         print(f'Could not read {path}')
     return s_and_p_df
