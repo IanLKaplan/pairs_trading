1cb08f4dd060c2e4b140a293eacccd9530890e7b
Ian Kaplan <iank@bearcave.com>
2022-08-10 17:57:08 -0400

    Added code to the MarketData class.

--- pairs_trading.py
+++ pairs_trading.py
@@ -2,8 +2,24 @@
 import os
 import pandas as pd
 from tabulate import tabulate
+from datetime import datetime
+from typing import List
+from pandas_datareader import data
+from datetime import timedelta
 
 s_and_p_file = 's_and_p_sector_components/sp_stocks.csv'
+s_and_p_data = 's_and_p_data'
+start_date_str = '2007-01-02'
+start_date: datetime = datetime.fromisoformat(start_date_str)
+
+
+def convert_date(some_date):
+    if type(some_date) == str:
+        some_date = datetime.fromisoformat(some_date)
+    elif type(some_date) == np.datetime64:
+        ts = (some_date - np.datetime64('1970-01-01T00:00')) / np.timedelta64(1, 's')
+        some_date = datetime.utcfromtimestamp(ts)
+    return some_date
 
 
 def read_stock_data(path: str) -> pd.DataFrame:
@@ -60,7 +76,60 @@ def calc_pair_counts(sector_info: dict) -> pd.DataFrame:
 
 
 stock_info_df = read_stock_data(s_and_p_file)
-sectors_df = extract_sectors(stock_info_df)
-info_df = calc_pair_counts(sectors_df)
+sectors = extract_sectors(stock_info_df)
+info_df = calc_pair_counts(sectors)
 
-print(tabulate(info_df, headers=[*info_df.columns], tablefmt='fancy_grid'))
+class MarketData:
+    def __init__(self, start_date: datetime, path: str):
+        self.start_date = start_date
+        self.path = path
+        self.end_date: datetime = datetime.today() - timedelta(days=1)
+
+    def get_market_data(self,
+                        symbols: List,
+                        start_date: datetime,
+                        end_date: datetime) -> pd.DataFrame:
+        data_source = 'yahoo'
+        data_col = 'Close'
+        if type(symbols) == str:
+            t = list()
+            t.append(symbols)
+            symbols = t
+        panel_data: pd.DataFrame = data.DataReader(symbols, data_source, start_date, end_date)
+        close_data: pd.DataFrame = panel_data[data_col]
+        assert len(close_data) > 0, f'Error reading data for {symbols}'
+        return close_data
+
+    def symbol_file_path(self, symbol: str) -> str:
+        path: str = self.path + os.path.sep + symbol.upper() + '.csv'
+        return path
+
+    def read_data(self, symbol: str) -> pd.DataFrame:
+        file_path = self.symbol_file_path(symbol)
+        if os.access(file_path, os.R_OK):
+            symbol_df = pd.read_csv(file_path, index_col='Date')
+            last_row = symbol_df.tail(1)
+            last_date = convert_date(last_row.index[0])
+            if last_date < self.end_date:
+                sym_start_date = last_date + timedelta(days=1)
+                new_data_df = self.get_market_data(symbol, sym_start_date, self.end_date)
+                symbol_df = pd.concat([symbol_df, new_data_df], axis=0)
+                symbol_df.to_csv(file_path)
+        else:
+            symbol_df = self.get_market_data(symbol, self.start_date, self.end_date)
+            if not os.access(self.path, os.R_OK):
+                os.mkdir(self.path)
+            symbol_df.to_csv(file_path)
+        return symbol_df
+
+    def get_close_data(self, stock_list: list) -> pd.DataFrame:
+        close_df = pd.DataFrame()
+        for stock in stock_list:
+            stock_df = self.read_data(stock)
+            close_df = pd.concat([close_df, stock_df], axis=1)
+        return close_df
+
+
+market_data = MarketData(start_date, s_and_p_data)
+xon_df = market_data.get_close_data(['XOM'])
+xon_df = market_data.get_close_data(['XOM'])
