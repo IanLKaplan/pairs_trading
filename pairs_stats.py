92f8949d29399a84f97e5a7e5589a5b01a1a0452
Ian Kaplan <ian@possiblefinance.com>
2022-10-18 17:10:03 -0400

    Added analysis code.

--- pairs_stats.py
+++ pairs_stats.py
@@ -1,8 +1,12 @@
 from datetime import datetime
 from typing import Tuple
 import matplotlib.pyplot as plt
+import statsmodels.api as sm
+from statsmodels.tsa.vector_ar.vecm import coint_johansen
 
 import pandas as pd
+import numpy as np
+from numpy import log
 from tabulate import tabulate
 
 from utils.find_date_index import findDateIndex
@@ -32,25 +36,82 @@ def normalize_df(data_df: pd.DataFrame) -> pd.DataFrame:
     return norm_df
 
 
-def add_mean(data_df: pd.DataFrame) -> pd.DataFrame:
-    mean_df = pd.DataFrame((data_df[data_df.columns[0]] + data_df[data_df.columns[1]]) / 2)
-    mean_df.columns = ['mean']
-    data_mean_df = pd.concat([data_df, mean_df], axis=1)
-    return data_mean_df
+def sum_df(close_df: pd.DataFrame) -> pd.DataFrame:
+    sum_df = close_df[close_df.columns[0]]
+    for i in range(0,len(close_df.columns)):
+        sum_df = sum_df + close_df[close_df.columns[i]]
+    return sum_df
 
 
-def add_stats(data_df: pd.DataFrame) -> pd.DataFrame:
-    mean_df = pd.DataFrame((data_df[data_df.columns[0]] + data_df[data_df.columns[1]]) / 2)
-    mean_df.columns = ['mean']
-    data_mean_df = add_mean(data_df)
-    mean_s = data_mean_df['mean']
-    stddev_s = mean_s.std()
-    stddev_high = pd.DataFrame(mean_s + stddev_s)
-    stddev_low = pd.DataFrame(mean_s - stddev_s)
-    stddev_high.columns = ['StdDev high']
-    stddev_low.columns = ['StdDev low']
-    data_stats_df = pd.concat([data_mean_df, stddev_high, stddev_low ], axis=1)
-    return data_stats_df
+def calc_mean(close_df: pd.DataFrame) -> float:
+    sum = sum_df(close_df)
+    ts_mean = sum / close_df.shape[1]
+    mean = ts_mean.mean()
+    return mean
+
+
+def running_mean(close_df: pd.DataFrame, window: int) -> pd.DataFrame:
+    sum = sum_df(close_df) / close_df.shape[1]
+    running_mean = pd.DataFrame(pd.Series(sum).rolling(window=window).mean().iloc[window:])
+    return running_mean
+
+
+def running_sd(close_df: pd.DataFrame, window: int) -> pd.DataFrame:
+    sum = sum_df(close_df) / close_df.shape[1]
+    running_sd = pd.DataFrame(pd.Series(sum).rolling(window=window).std().iloc[window:])
+    return running_sd
+
+
+def compute_halflife(prices_df: pd.DataFrame) -> int:
+    """
+    Calculate the half-life of a mean reverting series where the series
+    is a Ornstein–Uhlenbeck process
+    https://quant.stackexchange.com/a/25119
+    """
+    sum_df = prices_df[prices_df.columns[0]]
+    for i in range(0,len(prices_df.columns)):
+        sum_df = sum_df + prices_df[prices_df.columns[i]]
+    prices_a = sum_df.values
+    prices_lag = prices_a[1:]
+    prices_trunc = prices_a[0:-1]
+    prices_diff = prices_trunc - prices_lag
+    prices_lag_m = sm.add_constant(prices_lag)
+    res = sm.OLS(prices_diff, prices_lag_m).fit()
+    halflife = -log(2) / res.params[1]
+    halflife = int(round(halflife,0))
+    return halflife
+
+
+class RegressionResult:
+    def __init__(self,
+                 slope: pd.DataFrame,
+                 intercept: float,
+                 residuals: pd.Series):
+        self.slope = slope
+        self.intercept = intercept
+        self.residuals = residuals
+
+def least_squares(time_series_a: pd.DataFrame, time_series_b: pd.DataFrame):
+    times_series_b_const = sm.add_constant(time_series_b)
+    result_ab = sm.OLS(time_series_a, times_series_b_const).fit()
+    time_series_a_const = sm.add_constant(time_series_a)
+    result_ba = sm.OLS(time_series_b, time_series_a_const).fit()
+    stock_a = time_series_a.columns[0]
+    stock_b = time_series_b.columns[0]
+    slope_ab = pd.DataFrame([result_ab.params[stock_b]])
+    slope_ab.columns = [stock_b]
+    slope_ba = pd.DataFrame([result_ba.params[stock_a]])
+    slope_ba.columns = [stock_a]
+    result = result_ab
+    slope = slope_ab
+    if slope_ab.values[0] < slope_ba.values[0]:
+        result = result_ba
+        slope = slope_ba
+    intercept = round(result.params['const'], 2)
+    slope = round(slope, 2)
+    residuals = result.resid
+    rsltObj = RegressionResult(slope, intercept, residuals)
+    return rsltObj
 
 
 d2007_ix = 0
@@ -68,16 +129,37 @@ cor_df.columns = [f'correlation [{close_prices_df.columns[0]},{close_prices_df.c
 print()
 print(tabulate(cor_df, headers=[*cor_df.columns], tablefmt='fancy_grid'))
 
-scale = 0.75
+d2007_half_life = compute_halflife(d2007_close_df)
+d2008_half_life = compute_halflife(d2008_close_df)
 
-d2007_norm_df = normalize_df(d2007_close_df)
-d2007_stats_df = add_stats(d2007_norm_df)
-d2008_norm_df = normalize_df(d2008_close_df)
-d2008_stats_df = add_stats(d2008_norm_df)
+d2007_close_adj_df = close_prices_df.iloc[d2007_ix:d2007_ix + half_year + d2007_half_life]
+d2008_close_adj_df = close_prices_df.iloc[d2008_ix:d2008_ix + half_year + d2008_half_life]
 
-d2007_stats_df.plot(grid=True, title=f'normalized AAPL/MPWR and mean Jan 3, 2007', figsize=(10, 6))
+d2007_close_adj_norm_df = normalize_df(d2007_close_adj_df)
+d2008_close_adj_norm_df = normalize_df(d2007_close_adj_df)
+
+d2007_running_mean = running_mean(d2007_close_adj_norm_df, d2007_half_life)
+d2007_close_win_df = d2007_close_adj_norm_df[d2007_half_life:]
+d2007_running_sd = running_sd(d2007_close_adj_norm_df, d2007_half_life)
+d2007_sd_top = d2007_running_mean + d2007_running_sd
+d2007_sd_top.columns = ['StdDev Top']
+d2007_sd_bottom = d2007_running_mean - d2007_running_sd
+d2007_sd_bottom.columns = ['StdDev Bottom']
+d2007_running_mean.columns = ['mean']
+
+d2007_plot_df = pd.concat([d2007_close_win_df, d2007_sd_top, d2007_running_mean, d2007_sd_bottom], axis=1)
+d2007_plot_df.plot(grid=True, title=f'AAPL/MPWR and mean Jan 3, 2007', figsize=(10, 6))
 plt.show()
-d2008_stats_df.plot(grid=True, title=f'normalized AAPL/MPWR and mean Jan 3, 2008', figsize=(10, 6))
-plt.show()
+
+rslt = least_squares(pd.DataFrame(d2007_close_adj_df[d2008_close_adj_df.columns[0]]), pd.DataFrame(d2007_close_adj_df[d2008_close_adj_df.columns[1]]))
+
+johansen_rslt = coint_johansen(d2007_close_adj_df, 0, 1)
+hedge = pd.DataFrame(johansen_rslt.evec[0])
+hedge.columns = d2008_close_adj_df.columns
+critical_vals = pd.DataFrame(johansen_rslt.trace_stat_crit_vals[0])
+critical_vals.columns = ['90', '95', '99']
+trace_stat = johansen_rslt.trace_stat[0]
+
+
 
 pass
